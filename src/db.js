cde29ed931c420e46d1539476b415e60d9ce3c23
juan felipe velasquez <jvelasquezmuoz@yahoo.com>
2022-12-27 19:50:33 -0500

    correcion env

--- src/db.js
+++ src/db.js
@@ -1,10 +1,7 @@
-require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Pokemon`, {
   logging: false, // set to console.log to see the raw SQL queries
